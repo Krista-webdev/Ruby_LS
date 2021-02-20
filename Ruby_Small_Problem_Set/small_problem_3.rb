34ccea0590dd545fb108425010c443c4eb2b7d22
krista <kristaogle@outlook.com>
2021-02-20 16:36:54 -0500

    updated file to include alternative code using split

--- Ruby_Small_Problem_Set/small_problem_3.rb
+++ Ruby_Small_Problem_Set/small_problem_3.rb
@@ -1,6 +1,5 @@
 # List of Digits
 # Write a method that takes one argument, a positive integer, and returns a list of the digits in the number.
-# Had to use solution - I attempted .split but kept getting a NoMethodError???
 
 def digit_list(number)
   number.to_s.chars.map(&:to_i)
@@ -14,4 +13,9 @@ puts digit_list(444) == [4, 4, 4]             # => true
 =begin
   Our goal is to convert a number to a list of its digits. First, we convert the number to a string, then split it into an array of numeric characters. This array is almost what we want, but we need an array of numbers, not an array of strings. Enumerable#map comes to the rescue. You might find the (&:to_i) part weird, but this is Ruby syntactic sugar; it's shorthand for: 
   something.map { |char| char.to_i }
+
+  Code using .split:
+    def digit_list(num)
+      num.to_s.split('').map { |each| each.to_i }
+    end
 =end
