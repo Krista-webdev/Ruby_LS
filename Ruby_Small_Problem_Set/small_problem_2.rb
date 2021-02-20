c811102d8e396aaca7777a26942565518fd0447f
krista <kristaogle@outlook.com>
2021-02-20 15:58:22 -0500

    updated small_problem_2 file

--- Ruby_Small_Problem_Set/small_problem_2.rb
+++ Ruby_Small_Problem_Set/small_problem_2.rb
@@ -1,5 +1,7 @@
 # Odd
-# Write a method that takes one integer argument, which may be positive, negative, or zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
+=begin
+Write a method that takes one integer argument, which may be positive, negative, or zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
+=end
 
 def is_odd?(number)
   number % 2 == 1
