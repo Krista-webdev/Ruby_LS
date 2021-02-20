7ceafdf77a1143c8ab0634117d6cfec956698fb6
krista <kristaogle@outlook.com>
2021-02-20 15:45:52 -0500

    Updated basic_1.rb file

--- Ruby_Basics_Problem_Set/basic_1.rb
+++ Ruby_Basics_Problem_Set/basic_1.rb
@@ -1,1 +1,16 @@
-# LS Ruby Basics Problem Set
+# Problem 1 - Repeat Yourself
+
+def repeat(string, integer)
+  if integer.positive?
+    integer.times do
+      puts string
+    end
+  else
+    puts 'Integer must be a positive number.'
+  end
+end
+
+repeat('Hello', 3)
+repeat('Hello', 0)
+repeat('Hello', -1)
+repeat('Bye', 8)
