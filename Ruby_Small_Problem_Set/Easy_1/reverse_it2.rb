0b64c7e587c5fa2a6ebe42a1ebf83220f5a9ceab
krista <kristaogle@outlook.com>
2021-03-10 12:07:56 -0500

    Ruby Small Problems Easy 1 -  added reverse_it2.rb

---
+++ Ruby_Small_Problem_Set/Easy_1/reverse_it2.rb
@@ -0,0 +1,18 @@
+# Write a method that takes one argument, a string containing one or more words, and returns the given string with words that contain five or more characters reversed. Each string will consist of only letters and spaces. Spaces should be included only when more than one word is present.
+
+# Couldn't solve this one...
+
+def reverse_words(string)
+  words = []
+
+  string.split.each do |word|
+    word.reverse! if word.size >= 5
+    words << word
+  end
+
+  words.join(' ')
+end
+
+puts reverse_words('Professional')          # => lanoisseforP
+puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
+puts reverse_words('Launch School')         # => hcnuaL loohcS
