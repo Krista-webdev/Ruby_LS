68af6a78c9470b3999716503bc4d09df161ca4c9
krista <kristaogle@outlook.com>
2021-03-14 13:30:55 -0400

    updated code using better words for parameters

--- Ruby_Small_Problem_Set/Easy_1/my_bonus.rb
+++ Ruby_Small_Problem_Set/Easy_1/my_bonus.rb
@@ -1,7 +1,7 @@
 # Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
 
-def calculate_bonus(int, bool)
-  bool == true ? int / 2 : int = 0
+def calculate_bonus(salary, bonus)
+  bonus ? (salary / 2) : bonus = 0
 end
 
 puts calculate_bonus(2800, true) == 1400
