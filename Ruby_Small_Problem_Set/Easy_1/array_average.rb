12470a748136fab5a12713472de95546f323a5ab
krista <kristaogle@outlook.com>
2021-03-14 13:07:09 -0400

    updated file

--- Ruby_Small_Problem_Set/Easy_1/array_average.rb
+++ Ruby_Small_Problem_Set/Easy_1/array_average.rb
@@ -1,8 +1,7 @@
 # Write a method that takes one argument, an array containing integers, and returns the average of all numbers in the array. The array will never be empty and the numbers will always be positive integers. Your result should also be an integer.
 
 def average(arr)
-  num = arr.sum(0.0) / arr.size
-  num.floor
+  arr.sum / arr.size
 end
 
 puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3  => true
