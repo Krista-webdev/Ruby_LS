ff08885655567092c0e41fc7a7f385c3b86a846a
krista <kristaogle@outlook.com>
2021-03-14 13:24:11 -0400

    updated sum_of_digits.rb file

--- Ruby_Small_Problem_Set/Easy_1/sum_of_digits.rb
+++ Ruby_Small_Problem_Set/Easy_1/sum_of_digits.rb
@@ -2,11 +2,16 @@
 # For a challenge, try writing this without any basic looping constructs (while, until, loop, and each).
 
 def sum(int)
-  num = int.to_s.split('')
-  num.map! {|i| i.to_i}
+  num = int.to_s.split('').map {|i| i.to_i}
   num.sum
 end
 
 puts sum(23) == 5
 puts sum(496) == 19
 puts sum(123_456_789) == 45
+
+# Alternate Solution
+
+# def sum(int)
+#   int.to_s.split('').map(&:to_i).reduce(:+)
+# end
