a28b23edc26f09f07754ea2a07299f431ce5ae0f
krista <kristaogle@outlook.com>
2021-03-23 16:23:27 -0400

    updated file to include LS solution

--- Ruby_Small_Problem_Set/Easy_2/greet_user.rb
+++ Ruby_Small_Problem_Set/Easy_2/greet_user.rb
@@ -4,6 +4,28 @@ puts "What is your name?"
 name = gets.chomp
 
 if name.include?("!")
+  puts "HELLO #{name.upcase} WHY ARE YOU SCREAMING!!??"
+else
+  puts "Hello #{name}."
+end
+
+#LS Solution:
+print 'What is your name? '
+name = gets.chomp
+
+if name[-1] == '!'
+  name = name.chop
+  puts "HELLO #{name.upcase}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
+
+# Try modifying our solution to use String#chomp! and String#chop!, respectively.
+puts "What is your name?"
+name = gets.chomp!
+
+if name[-1] == "!"
+  name = name.chop!
   puts "HELLO #{name.upcase}! WHY ARE YOU SCREAMING!!??"
 else
   puts "Hello #{name}."
