5b4a4f659a97fc75eb0afae58180ac7a4cc200cb
krista <kristaogle@outlook.com>
2021-03-24 12:00:52 -0400

    updated file to include LS solution

--- Ruby_Small_Problem_Set/Easy_2/odd_numbers.rb
+++ Ruby_Small_Problem_Set/Easy_2/odd_numbers.rb
@@ -1,3 +1,10 @@
 # Print all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
 
 (1..99).each { |n| puts n if n.odd?}
+
+#LS Solution
+value = 1
+while value <= 99
+  puts value
+  value += 2
+end
