0e805b572a55837e0f514f1b9c168ec67537ce33
Krista-webdev <kristaogle@outlook.com>
2021-04-02 15:57:43 -0400

    commit file

---
+++ Ruby_Small_Problem_Set/Easy_3/palindromic_string.rb
@@ -0,0 +1,14 @@
+# Write a method that returns true if the string passed as an argument is a palindrome, false otherwise. A palindrome reads the same forward and backward. For this exercise, case matters as does punctuation and spaces.
+
+def palindrome?(word)
+  word == word.reverse
+end
+
+palindrome?('madam') == true
+palindrome?('Madam') == false          # (case matters)
+palindrome?("madam i'm adam") == false # (all characters matter)
+palindrome?('356653') == true
+
+# Further Exploration:
+# Write a method that determines whether an array is palindromic; that is, the element values appear in the same sequence both forwards and backwards in the array. Now write a method that determines whether an array or a string is palindromic; that is, write a method that can take either an array or a string argument, and determines whether that argument is a palindrome. You may not use an if, unless, or case statement or modifier.
+# Will complete this when I review for the 109 Assessment
