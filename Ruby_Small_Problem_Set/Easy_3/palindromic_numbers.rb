a34d08da5bdd804689074add4a89f481bc6da369
Krista-webdev <kristaogle@outlook.com>
2021-04-02 16:34:30 -0400

    commit file

---
+++ Ruby_Small_Problem_Set/Easy_3/palindromic_numbers.rb
@@ -0,0 +1,21 @@
+# Write a method that returns true if its integer argument is palindromic, false otherwise. A palindromic number reads the same forwards and backwards.
+
+def palindromic_number?(number)
+  number.to_s == number.to_s.reverse
+end
+
+palindromic_number?(34543) == true
+palindromic_number?(123210) == false
+palindromic_number?(22) == true
+palindromic_number?(5) == true
+
+# --------------------------------------
+
+# using palindrome? method:
+def palindrome?(string)
+  string == string.reverse
+end
+
+def palindromic_number?(number)
+  palindrome?(number.to_s)
+end
